fa4d351eb4b72737c5740fec165f0b1614c5acb9
Snehanewuser <32169446+Snehanewuser@users.noreply.github.com>
2018-05-14 12:50:47 +0530

    Update relay42_libmodules.py

--- relay42_automation/relay_42/relay42_libmodules.py
+++ relay42_automation/relay_42/relay42_libmodules.py
@@ -1,22 +1,17 @@
 ###########################################################################################
 # relay42 test automation demonstration
-                   #
 #
-                   #
+#
 # (C) 2018 by Sneha Mirajkar (sneha.mirajkar@gmail.com)
-                   #
+#
 # Can be copied for any purposes, Please get prior approval.
-                   #
 #
-                   #
-# The relya42_libmodules.py the library function reporting function
-used to log           #
-# messages(info about teststeps and the results of the testcasesin the
-                   #
-#/tmp/relay42_results/reportyfile.txt. This function is callable from
-all other functions #
 #
-                   #
+#
+# The relya42_libmodules.py the library function reporting function is
+# used to log messages(info about teststeps and the results of the testcases in the
+# /tmp/relay42_results/reportyfile.txt. This function is callable from all other functions
+#
 ###########################################################################################
 
 #!/usr/bin/env python
