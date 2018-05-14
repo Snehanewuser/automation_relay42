59788bd9303c1702cf48425aeb91f31b1dbba8b3
Snehanewuser <32169446+Snehanewuser@users.noreply.github.com>
2018-05-14 12:14:48 +0530

    Update relay42_main.py

--- relay42_automation/relay_42/relay42_main.py
+++ relay42_automation/relay_42/relay42_main.py
@@ -1,3 +1,26 @@
+###########################################################################################
+# relay42 test automation demonstration
+                   #
+#
+                   #
+# (C) 2018 by Sneha Mirajkar (sneha.mirajkar@gmail.com)
+                   #
+# Can be copied for any purposes, Please get prior approval.
+                   #
+#
+                   #
+# The relya42_main.py generates the runtime username, password,
+engagement-id to be passed#
+# to the functions called from  the relay42_usermodules.py as
+mentioned below             #
+# create engagement create audience and
+send_request_and_validate_audience-entry          #
+#
+                   #
+###########################################################################################
+
+
+
 #!/usr/bin/env python
 
 from relay42_usermodules import *
@@ -10,14 +33,15 @@ import time
 
 newscript ="*******************************************************************************"
 report_status(newscript)
-engagement = "Tomato_" + ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(3)])
+engagement = "Tomato_" + ''.join([random.choice(string.ascii_letters +
+string.digits) for n in xrange(3)])
+username="relay42test1@gmail.com"
+password="relay42test"
 report_status(engagement)
-engagement_link = create_engagement(engagement)
+engagement_link = create_engagement(username, password, engagement)
 report_status(engagement_link)
-return_audience_link=create_audience(engagement)
-send_req_n_validate_audi_entry(return_audience_link, engagement)
+return_audience_link=create_audience(username, password, engagement)
+send_req_n_validate_audi_entry(username, password,
+return_audience_link, engagement)
 print "All testcases are executed, please check/tmp/relay42_results folder"
 exit()
-
-
-
